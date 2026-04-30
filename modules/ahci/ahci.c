b119e6c3221f06b949bd80e4e4004c8673423e3d
yucarp <havalibiradres@gmail.com>
2026-04-30 14:40:05 +0300

    Add files via upload

--- modules/ahci/ahci.c
+++ modules/ahci/ahci.c
@@ -6,86 +6,113 @@
 #include <kernel/sbd.h>
 
 struct HBA_Port {
-    uint32_t command_list_address;
-    uint32_t cla_upper;
-    uint32_t fis_address;
-    uint32_t fis_upper;
-    uint32_t interrupt_status;
-    uint32_t interrupt_enable;
-    uint32_t command_and_status;
-    uint32_t reserved_dword;
-    uint32_t task_file_data;
-    uint32_t signature;
-    uint32_t sata_status;
-    uint32_t sata_control;
-    uint32_t sata_error;
-    uint32_t sata_active;
-    uint32_t command_issue;
-    uint32_t sata_notificiation;
-    uint32_t switch_control;
-    uint32_t reserved[11];
-    uint32_t vendor_specific[4];
+    volatile uint32_t command_list_address;
+    volatile uint32_t cla_upper;
+    volatile uint32_t fis_address;
+    volatile uint32_t fis_upper;
+    volatile uint32_t interrupt_status;
+    volatile uint32_t interrupt_enable;
+    volatile uint32_t command_and_status;
+    volatile uint32_t reserved_dword;
+    volatile uint32_t task_file_data;
+    volatile uint32_t signature;
+    volatile uint32_t sata_status;
+    volatile uint32_t sata_control;
+    volatile uint32_t sata_error;
+    volatile uint32_t sata_active;
+    volatile uint32_t command_issue;
+    volatile uint32_t sata_notificiation;
+    volatile uint32_t switch_control;
+    volatile uint32_t reserved[11];
+    volatile uint32_t vendor_specific[4];
 }__attribute__((packed));
 
 struct HBA_MemoryRegister {
-    uint32_t host_capability;
-    uint32_t global_host_control;
-    uint32_t interrupt_status;
-    uint32_t port_implemented;
-    uint32_t version;
-    uint32_t ccc_control;
-    uint32_t ccc_ports;
-    uint32_t em_location;
-    uint32_t em_control;
-    uint32_t capabilities_extended;
-    uint32_t bios_os_handoff_control;
-    uint8_t reserved[0xA0 - 0x2C];
-    uint8_t vendor_specific[0x100 - 0xA0];
-    struct HBA_Port hba_port[32];
+    volatile uint32_t host_capability;
+    volatile uint32_t global_host_control;
+    volatile uint32_t interrupt_status;
+    volatile uint32_t port_implemented;
+    volatile uint32_t version;
+    volatile uint32_t ccc_control;
+    volatile uint32_t ccc_ports;
+    volatile uint32_t em_location;
+    volatile uint32_t em_control;
+    volatile uint32_t capabilities_extended;
+    volatile uint32_t bios_os_handoff_control;
+    volatile uint8_t reserved[0xA0 - 0x2C];
+    volatile uint8_t vendor_specific[0x100 - 0xA0];
+    volatile struct HBA_Port hba_port[32];
 }__attribute__((packed));
 
 struct HBA_Command_Header{
-    uint8_t header_1;
-    uint8_t header_2;
-    uint16_t prd_table_length;
-    uint32_t prd_byte_count;
-    uint32_t ctd_base_address;
-    uint32_t ctd_base_upper;
-    uint32_t reserved[4];
+    volatile uint8_t header_1;
+    volatile uint8_t header_2;
+    volatile uint16_t prd_table_length;
+    volatile uint32_t prd_byte_count;
+    volatile uint32_t ctd_base_address;
+    volatile uint32_t ctd_base_upper;
+    volatile uint32_t reserved[4];
 }__attribute__((packed));
 
 struct HBA_Physical_Region_Descriptor_Entry {
-    uint32_t data_base_address;
-    uint32_t data_base_upper;
-    uint32_t reserved;
-    uint32_t byte_count_and_interrupt_on_completion;
+    volatile uint32_t data_base_address;
+    volatile uint32_t data_base_upper;
+    volatile uint32_t reserved;
+    volatile uint32_t byte_count_and_interrupt_on_completion;
 }__attribute__((packed));
 
 struct HBA_Command_Table {
-    uint8_t command_fis[64];
-    uint8_t atapi_command[16];
-    uint8_t reserved[48];
+    volatile uint8_t command_fis[64];
+    volatile uint8_t atapi_command[16];
+    volatile uint8_t reserved[48];
     struct HBA_Physical_Region_Descriptor_Entry hda_prdt[1];
 }__attribute__((packed));
 
 struct FIS_Register_H2D{
-    uint8_t fis_type; //Always 0x27
-    uint8_t multiplier_and_cc;
-    uint8_t command;
-    uint8_t feature;
-    uint8_t lba0;
-    uint8_t lba1;
-    uint8_t lba2;
-    uint8_t device;
-    uint8_t lba3;
-    uint8_t lba4;
-    uint8_t lba5;
-    uint8_t feature_high;
-    uint16_t count;
-    uint8_t icc;
-    uint8_t control;
-    uint32_t reserved[4];
+    volatile uint8_t fis_type; //Always 0x27
+    volatile uint8_t multiplier_and_cc;
+    volatile uint8_t command;
+    volatile uint8_t feature;
+    volatile uint8_t lba0;
+    volatile uint8_t lba1;
+    volatile uint8_t lba2;
+    volatile uint8_t device;
+    volatile uint8_t lba3;
+    volatile uint8_t lba4;
+    volatile uint8_t lba5;
+    volatile uint8_t feature_high;
+    volatile uint16_t count;
+    volatile uint8_t icc;
+    volatile uint8_t control;
+    volatile uint32_t reserved[4];
 }__attribute__((packed));
+
+struct FIS_Register_PIO {
+    volatile uint8_t fis_type;
+    volatile uint8_t header;
+    volatile uint8_t status;
+    volatile uint8_t error;
+    volatile uint8_t lba0;
+    volatile uint8_t lba1;
+    volatile uint8_t lba2;
+    volatile uint8_t device;
+    volatile uint8_t lba3;
+    volatile uint8_t lba4;
+    volatile uint8_t lba5;
+    volatile uint8_t reserved1;
+    volatile uint16_t count;
+    volatile uint8_t reserved2;
+    volatile uint8_t new_status;
+    volatile uint16_t transfer_count;
+    volatile uint16_t reserved3;
+};
+
+struct FIS_Register_Data {
+    volatile uint8_t fis_type; //Always 0x46
+    volatile uint8_t port_multiplier;
+    volatile uint16_t reserved;
+    volatile uint32_t data[1];
+};
 
 void InitializeAhciPort(struct HBA_Port *port, int port_num){
     port->command_and_status &= ~0x1;
@@ -95,24 +122,21 @@ void InitializeAhciPort(struct HBA_Port *port, int port_num){
 
     for(int i = 0; i < 0x40000; i += 0x1000){
         KernelMapMmio(ahci_base + i, ahci_base + i);
-        if(KernelAllocateFrame() != ahci_base + i + 0x1000) break;
+        if(KernelAllocateFrame() != ahci_base + i + 0x1000) continue;
     }
 
     while(1){
         if((port->command_and_status & 0x4000) || (port->command_and_status & 0x8000)) continue;
         break;
     }
-
-    uintptr_t command_port_address = ahci_base + (port_num << 10) + KernelGetHhdmOffset();
+;
     port->command_list_address = ahci_base + (port_num << 10);
     port->cla_upper = 0;
-
-    uintptr_t fis_address = ahci_base + (32 << 10) + (port_num << 8) + KernelGetHhdmOffset();
     port->fis_address = ahci_base + (32 << 10) + (port_num << 8);
     port->fis_upper = 0;
-    memset((void *)fis_address, 0, 256);
+    memset((void *)port->fis_address, 0, 256);
 
-    struct HBA_Command_Header *command_header = (struct HBA_Command_Header *)(command_port_address);
+    struct HBA_Command_Header *command_header = (struct HBA_Command_Header *)(port->command_list_address);
     for(int i = 0; i < 32; ++i){
         command_header[i].prd_table_length = 8;
         command_header[i].ctd_base_address = ahci_base + (40 << 10) + (port_num << 13) + (i << 8);
@@ -125,6 +149,7 @@ void InitializeAhciPort(struct HBA_Port *port, int port_num){
 
     port->command_and_status |= 0x1;
     port->command_and_status |= 0x10;
+
 }
 int AhciFindFreeCommandSlot(struct HBA_Port *port){
     uint32_t slots = (port->sata_active | port->command_issue);
@@ -141,33 +166,25 @@ void AhciSendIdentify(struct HBA_Port *port){
     int slot = AhciFindFreeCommandSlot(port);
     if(slot == -1) return;
 
-    struct HBA_Command_Header *command_header = (struct HBA_Command_Header *)(port->command_list_address + KernelGetHhdmOffset());
+    struct HBA_Command_Header *command_header = (struct HBA_Command_Header *)(port->command_list_address);
     command_header += slot;
     command_header->header_1 |= (sizeof(struct FIS_Register_H2D) / sizeof(uint32_t)) & 0x1F;
     command_header->header_1 &= ~(1 << 6);
     command_header->prd_table_length = 1;
-    struct HBA_Command_Table *command_table = (struct HBA_Command_Table *)(command_header->ctd_base_address + KernelGetHhdmOffset());
+    struct HBA_Command_Table *command_table = (struct HBA_Command_Table *)(command_header->ctd_base_address);
     memset(command_table, 0, sizeof(struct HBA_Command_Table) + sizeof(struct HBA_Physical_Region_Descriptor_Entry));
     int i = 0;
-    for(; i < (command_header->header_1 & 0x1F); ++i){
-        command_table->hda_prdt[i].data_base_address = (uint64_t) &buffer & 0xFFFFFFFF;
-        command_table->hda_prdt[i].data_base_upper = ((uint64_t)&buffer >> 32) & 0xFFFFFFFF;
-        command_table->hda_prdt[i].byte_count_and_interrupt_on_completion |= 8192;
-        command_table->hda_prdt[i].byte_count_and_interrupt_on_completion |= (1 << 31);
-    }
+
+    command_table->hda_prdt[i].data_base_address = (uint64_t) &buffer & 0xFFFFFFFF;
+    command_table->hda_prdt[i].data_base_upper = ((uint64_t)&buffer >> 32) & 0xFFFFFFFF;
+    command_table->hda_prdt[i].byte_count_and_interrupt_on_completion |= 511;
+    command_table->hda_prdt[i].byte_count_and_interrupt_on_completion |= (1 << 31);
 
     struct FIS_Register_H2D *command_fis = (struct FIS_Register_H2D *)(&command_table->command_fis);
     command_fis->fis_type = 0x27;
     command_fis->multiplier_and_cc |= 1 << 7;
     command_fis->command = 0xEC;
 
-    command_fis->lba0 = 0;
-    command_fis->lba1 = 0;
-    command_fis->lba2 = 0;
-    command_fis->lba3 = 0;
-    command_fis->lba4 = 0;
-    command_fis->lba5 = 0;
-    command_fis->count = 0;
     command_fis->device = 0;
 
     if(port->task_file_data & 0x1){
@@ -178,17 +195,14 @@ void AhciSendIdentify(struct HBA_Port *port){
     //while(port->task_file_data & (0x88));
     port->command_issue = 1 << slot;
 
-    /*while(1){
-        if((port->command_issue & (1 << slot)) == 0) break;
-        if(port->interrupt_status & 0x1){
-            kprint("Error");
-            return;
-        }
-    }*/
+    struct FIS_Register_Data fis_pio = {0};
+    memcpy(&fis_pio, (void *)((uint64_t)port->fis_address + 0x20), 64);
+    kprint("FIS type: %x\n", fis_pio.fis_type);
 
-    for(int i = 0; i < 4096; ++i){
-        if(buffer[i]) kprint("Buffer: %x\n", buffer[i]);
-    }
+    while(!((port->command_and_status >> 15) & 1));
+    struct FIS_Register_Data *fis_data = (struct FIS_Register_Data *)((uint64_t)port->fis_address + 0x60);
+    kprint("FIS type: %x\n", fis_data->fis_type);
+    while(1) asm("hlt");
 }
 
 void InitializeAhci(){
